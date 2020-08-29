e42672807a51d928bc9ddc8732d5621c442879d0
Nick Williams <git@nickawilliams.com>
2020-08-28 19:01:41 -0700

    Chore: Added tests.

---
+++ src/index.test.ts
@@ -0,0 +1,402 @@
+import Try, { Exception, Runner, TryBlock } from './'
+
+class ExceptionOne extends Exception {}
+class ExceptionTwo extends Exception {}
+class ExceptionThree extends Exception {}
+class AnotherError extends Error {}
+
+describe('Try().Done()', () => {
+  test('Just runs when no error is thrown.', () => {
+    const tryBlock: TryBlock = jest.fn()
+
+    Try(tryBlock).Done()
+
+    expect(tryBlock).toHaveBeenCalled()
+  })
+
+  test('Results in an uncaught(ExceptionOne) when a matching ExceptionOne is thrown.', () => {
+    const tryBlock: TryBlock = jest.fn(() => {
+      throw new ExceptionOne('Wat?')
+    })
+
+    expect(() => Try(tryBlock).Done()).toThrow(ExceptionOne)
+
+    expect(tryBlock).toHaveBeenCalled()
+  })
+})
+
+describe('Try().Catch(ExceptionOne)).Done()', () => {
+  test('Just runs when no error is thrown.', () => {
+    const tryBlock = jest.fn()
+    const catchBlock = jest.fn()
+
+    Try(tryBlock)
+      .Catch(ExceptionOne, catchBlock)
+      .Done()
+
+    expect(tryBlock).toHaveBeenCalled()
+    expect(catchBlock).not.toHaveBeenCalled()
+  })
+
+  test('Results in an uncaught ExceptionTwo when a non-matching ExceptionTwo is thrown.', () => {
+    const tryBlock = jest.fn(() => {
+      throw new ExceptionTwo('Waat?')
+    })
+    const catchBlock = jest.fn()
+
+    expect(() =>
+      Try(tryBlock)
+        .Catch(ExceptionOne, catchBlock)
+        .Done()
+    ).toThrow(ExceptionTwo)
+
+    expect(tryBlock).toHaveBeenCalled()
+    expect(catchBlock).not.toHaveBeenCalled()
+  })
+
+  test('Catch(ExceptionOne) is invoked when a matching error is thrown.', () => {
+    const tryBlock = jest.fn(() => {
+      throw new ExceptionOne('Wat?')
+    })
+    const catchBlock = jest.fn()
+
+    expect(() =>
+      Try(tryBlock)
+        .Catch(ExceptionOne, catchBlock)
+        .Done()
+    ).not.toThrow()
+
+    expect(tryBlock).toHaveBeenCalled()
+    expect(catchBlock).toHaveBeenCalled()
+  })
+})
+
+describe('Try().Catch(ExceptionOne)).Catch(ExceptionTwo).Done()', () => {
+  test('Just runs when no error is thrown.', () => {
+    const tryBlock = jest.fn()
+    const catchBlockOne = jest.fn()
+    const catchBlockTwo = jest.fn()
+
+    Try(tryBlock)
+      .Catch(ExceptionOne, catchBlockOne)
+      .Catch(ExceptionTwo, catchBlockTwo)
+      .Done()
+
+    expect(tryBlock).toHaveBeenCalled()
+    expect(catchBlockOne).not.toHaveBeenCalled()
+    expect(catchBlockTwo).not.toHaveBeenCalled()
+  })
+
+  test('Results in an uncaught ExceptionThree when a non-matching ErrorThree is thrown.', () => {
+    const tryBlock = jest.fn(() => {
+      throw new ExceptionThree('Waaat?')
+    })
+    const catchBlockOne = jest.fn()
+    const catchBlockTwo = jest.fn()
+
+    expect(() => {
+      Try(tryBlock)
+        .Catch(ExceptionOne, catchBlockOne)
+        .Catch(ExceptionTwo, catchBlockTwo)
+        .Done()
+    }).toThrow(ExceptionThree)
+
+    expect(tryBlock).toHaveBeenCalled()
+    expect(catchBlockOne).not.toHaveBeenCalled()
+    expect(catchBlockTwo).not.toHaveBeenCalled()
+  })
+
+  test('Catch(ExceptionOne)) is invoked when a matching(ExceptionOne) is thrown. Catch(ExceptionTwo) is not invoked.', () => {
+    const tryBlock = jest.fn(() => {
+      throw new ExceptionOne('Wat?')
+    })
+    const catchBlockOne = jest.fn()
+    const catchBlockTwo = jest.fn()
+
+    Try(tryBlock)
+      .Catch(ExceptionOne, catchBlockOne)
+      .Catch(ExceptionTwo, catchBlockTwo)
+      .Done()
+
+    expect(tryBlock).toHaveBeenCalled()
+    expect(catchBlockOne).toHaveBeenCalled()
+    expect(catchBlockTwo).not.toHaveBeenCalled()
+  })
+
+  test('Catch(ExceptionTwo) is invoked when a matching ExceptionTwo is thrown. Catch(ExceptionOne)) is not invoked.', () => {
+    const tryBlock = jest.fn(() => {
+      throw new ExceptionTwo('Waat?')
+    })
+    const catchBlockOne = jest.fn()
+    const catchBlockTwo = jest.fn()
+
+    Try(tryBlock)
+      .Catch(ExceptionOne, catchBlockOne)
+      .Catch(ExceptionTwo, catchBlockTwo)
+      .Done()
+
+    expect(tryBlock).toHaveBeenCalled()
+    expect(catchBlockOne).not.toHaveBeenCalled()
+    expect(catchBlockTwo).toHaveBeenCalled()
+  })
+})
+
+describe('Try().Finally().Done()', () => {
+  test('Just runs when no error is thrown. Finally is invoked.', () => {
+    const tryBlock = jest.fn()
+    const finallyBlock = jest.fn()
+
+    Try(tryBlock)
+      .Finally(finallyBlock)
+      .Done()
+
+    expect(tryBlock).toHaveBeenCalled()
+    expect(finallyBlock).toHaveBeenCalled()
+  })
+
+  test('Results in an uncaught ExceptionOne when an ExceptionOne is thrown. Finally is invoked.', () => {
+    const tryBlock = jest.fn(() => {
+      throw new ExceptionOne('Wat?')
+    })
+    const finallyBlock = jest.fn()
+
+    expect(() => {
+      Try(tryBlock)
+        .Finally(finallyBlock)
+        .Done()
+    }).toThrow(ExceptionOne)
+
+    expect(tryBlock).toHaveBeenCalled()
+    expect(finallyBlock).toHaveBeenCalled()
+  })
+})
+
+describe('Try().Catch(ExceptionOne)).Finally().Done()', () => {
+  test('Just runs when no error is thrown. Finally is invoked', () => {
+    const tryBlock = jest.fn()
+    const catchBlock = jest.fn()
+    const finallyBlock = jest.fn()
+
+    Try(tryBlock)
+      .Catch(ExceptionOne, catchBlock)
+      .Finally(finallyBlock)
+      .Done()
+
+    expect(tryBlock).toHaveBeenCalled()
+    expect(catchBlock).not.toHaveBeenCalled()
+    expect(finallyBlock).toHaveBeenCalled()
+  })
+
+  test('Results in an uncaught ExceptionTwo when a non-matching ExceptionTwo is thrown. Finally is invoked.', () => {
+    const tryBlock = jest.fn(() => {
+      throw new ExceptionTwo('Waat?')
+    })
+    const catchBlock = jest.fn()
+    const finallyBlock = jest.fn()
+
+    expect(() => {
+      Try(tryBlock)
+        .Catch(ExceptionOne, catchBlock)
+        .Finally(finallyBlock)
+        .Done()
+    }).toThrow(ExceptionTwo)
+
+    expect(tryBlock).toHaveBeenCalled()
+    expect(catchBlock).not.toHaveBeenCalled()
+    expect(finallyBlock).toHaveBeenCalled()
+  })
+
+  test('Catch(ExceptionOne)) is invoked when a matching ExceptionOne is thrown. Finally is invoked.', () => {
+    const tryBlock = jest.fn(() => {
+      throw new ExceptionOne('Wat?')
+    })
+    const catchBlock = jest.fn()
+    const finallyBlock = jest.fn()
+
+    Try(tryBlock)
+      .Catch(ExceptionOne, catchBlock)
+      .Finally(finallyBlock)
+      .Done()
+
+    expect(tryBlock).toHaveBeenCalled()
+    expect(catchBlock).toHaveBeenCalled()
+    expect(finallyBlock).toHaveBeenCalled()
+  })
+})
+
+describe('Try().Catch(ExceptionOne)).Catch(ExceptionTwo).Finally().Done()', () => {
+  test('Just runs when no error is thrown. Finally is invoked.', () => {
+    const tryBlock = jest.fn()
+    const catchBlockOne = jest.fn()
+    const catchBlockTwo = jest.fn()
+    const finallyBlock = jest.fn()
+
+    Try(tryBlock)
+      .Catch(ExceptionOne, catchBlockOne)
+      .Catch(ExceptionTwo, catchBlockTwo)
+      .Finally(finallyBlock)
+      .Done()
+
+    expect(tryBlock).toHaveBeenCalled()
+    expect(catchBlockOne).not.toHaveBeenCalled()
+    expect(catchBlockTwo).not.toHaveBeenCalled()
+    expect(finallyBlock).toHaveBeenCalled()
+  })
+
+  test('Results in an uncaught ErrorThree when a  non-matching ErrorThree is thrown. Finally is invoked.', () => {
+    const tryBlock = jest.fn(() => {
+      throw new ExceptionThree('Waaat?')
+    })
+    const catchBlockOne = jest.fn()
+    const catchBlockTwo = jest.fn()
+    const finallyBlock = jest.fn()
+
+    expect(() => {
+      Try(tryBlock)
+        .Catch(ExceptionOne, catchBlockOne)
+        .Catch(ExceptionTwo, catchBlockTwo)
+        .Finally(finallyBlock)
+        .Done()
+    }).toThrow(ExceptionThree)
+
+    expect(tryBlock).toHaveBeenCalled()
+    expect(catchBlockOne).not.toHaveBeenCalled()
+    expect(catchBlockTwo).not.toHaveBeenCalled()
+    expect(finallyBlock).toHaveBeenCalled()
+  })
+
+  test('Catch(ExceptionOne)) is invoked when a matching ExceptionOne  is thrown. Catch(ExceptionTwo) is not invoked. Finally is invoked.', () => {
+    const tryBlock = jest.fn(() => {
+      throw new ExceptionOne('Wat?')
+    })
+    const catchBlockOne = jest.fn()
+    const catchBlockTwo = jest.fn()
+    const finallyBlock = jest.fn()
+
+    Try(tryBlock)
+      .Catch(ExceptionOne, catchBlockOne)
+      .Catch(ExceptionTwo, catchBlockTwo)
+      .Finally(finallyBlock)
+      .Done()
+
+    expect(tryBlock).toHaveBeenCalled()
+    expect(catchBlockOne).toHaveBeenCalled()
+    expect(catchBlockTwo).not.toHaveBeenCalled()
+    expect(finallyBlock).toHaveBeenCalled()
+  })
+
+  test('Catch(ExceptionTwo) is invoked when a matching ExceptionTwo is thrown. Catch(ExceptionOne)) is not invoked. Finally is invoked.', () => {
+    const tryBlock = jest.fn(() => {
+      throw new ExceptionTwo('Wat?')
+    })
+    const catchBlockOne = jest.fn()
+    const catchBlockTwo = jest.fn()
+    const finallyBlock = jest.fn()
+
+    Try(tryBlock)
+      .Catch(ExceptionOne, catchBlockOne)
+      .Catch(ExceptionTwo, catchBlockTwo)
+      .Finally(finallyBlock)
+      .Done()
+
+    expect(tryBlock).toHaveBeenCalled()
+    expect(catchBlockOne).not.toHaveBeenCalled()
+    expect(catchBlockTwo).toHaveBeenCalled()
+    expect(finallyBlock).toHaveBeenCalled()
+  })
+})
+
+describe('Try().Catch(ExceptionOne)).Catch(ExceptionTwo).Finally()', () => {
+  test('Nothing happens because Done was not called.', () => {
+    const tryBlock = jest.fn()
+    const catchBlockOne = jest.fn()
+    const catchBlockTwo = jest.fn()
+    const finallyBlock = jest.fn()
+
+    Try(tryBlock)
+      .Catch(ExceptionOne, catchBlockOne)
+      .Catch(ExceptionTwo, catchBlockTwo)
+      .Finally(finallyBlock)
+
+    expect(tryBlock).not.toHaveBeenCalled()
+    expect(catchBlockOne).not.toHaveBeenCalled()
+    expect(catchBlockTwo).not.toHaveBeenCalled()
+    expect(finallyBlock).not.toHaveBeenCalled()
+  })
+})
+
+describe('Try().Catch(Exception).Catch(ExceptionOne)).Catch(ExceptionTwo).Finally().Done()', () => {
+  test('Just runs when no error is thrown. Finally is invoked.', () => {
+    const tryBlock = jest.fn()
+    const catchBlockOne = jest.fn()
+    const catchBlockTwo = jest.fn()
+    const finallyBlock = jest.fn()
+
+    Try(tryBlock)
+      .Catch(ExceptionOne, catchBlockOne)
+      .Catch(ExceptionTwo, catchBlockTwo)
+      .Finally(finallyBlock)
+      .Done()
+
+    expect(tryBlock).toHaveBeenCalled()
+    expect(catchBlockOne).not.toHaveBeenCalled()
+    expect(catchBlockTwo).not.toHaveBeenCalled()
+    expect(finallyBlock).toHaveBeenCalled()
+  })
+
+  test('Catch(Exception) is invoked when a matching subclass ExceptionOne is thrown. Catch(ExceptionTwo) is not invoked. Finally is invoked.', () => {
+    const tryBlock = jest.fn(() => {
+      throw new ExceptionOne('Wat?')
+    })
+    const catchBlockOne = jest.fn()
+    const catchBlockTwo = jest.fn()
+    const finallyBlock = jest.fn()
+
+    Try(tryBlock)
+      .Catch(Exception, catchBlockOne)
+      .Catch(ExceptionTwo, catchBlockTwo)
+      .Finally(finallyBlock)
+      .Done()
+
+    expect(tryBlock).toHaveBeenCalled()
+    expect(catchBlockOne).toHaveBeenCalled()
+    expect(catchBlockTwo).not.toHaveBeenCalled()
+    expect(finallyBlock).toHaveBeenCalled()
+  })
+})
+
+describe('Try().Catch(AnotherError).Finally().Done()', () => {
+  test('Catch(AnotherError) is invoked when a subclass of Error is thrown. Finally is invoked.', () => {
+    const tryBlock = jest.fn(() => {
+      throw new AnotherError('Say whaaaaat?')
+    })
+    const catchBlock = jest.fn()
+    const finallyBlock = jest.fn()
+
+    Try(tryBlock)
+      .Catch(AnotherError, catchBlock)
+      .Finally(finallyBlock)
+      .Done()
+
+    expect(tryBlock).toHaveBeenCalled()
+    expect(catchBlock).toHaveBeenCalled()
+    expect(finallyBlock).toHaveBeenCalled()
+  })
+
+  test('Catch(Error) is invoked when a matching subclass AnotherError is thrown. Finally is invoked.', () => {
+    const tryBlock = jest.fn(() => {
+      throw new AnotherError('Say whaaaaat?')
+    })
+    const catchBlock = jest.fn()
+    const finallyBlock = jest.fn()
+
+    Try(tryBlock)
+      .Catch(Error, catchBlock)
+      .Finally(finallyBlock)
+      .Done()
+
+    expect(tryBlock).toHaveBeenCalled()
+    expect(catchBlock).toHaveBeenCalled()
+    expect(finallyBlock).toHaveBeenCalled()
+  })
+})
